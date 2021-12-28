573f6ccf8d5f71f1b729d4fe0cedee107c5fae17
Laith Alzoubi <55361207+allaith7@users.noreply.github.com>
2021-12-27 21:07:17 -0500

    Shop App

---
+++ shop/lib/models/http_exception.dart
@@ -0,0 +1,18 @@
+
+
+
+
+class HttpException implements Exception{
+  final String message;
+
+  HttpException(this.message);
+
+  @override
+  String toString(){
+
+    return message;
+
+    // return super.toString();
+  }
+}
+
